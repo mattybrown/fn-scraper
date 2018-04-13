57363d29506f3a32c485e6a6fc2fca4eb05f21e7
Matt Brown <brownprobable@gmail.com>
2018-04-13 13:02:32 +1200

    scraper can handle if values are not present or different

--- scraper.rb
+++ scraper.rb
@@ -5,18 +5,23 @@ require 'open-uri'
 require 'json'
 
 #doc = File.open("example.html") { |f| Nokogiri::HTML(f) }
-doc = Nokogiri::HTML(open('http://marlboroughrealestate.co.nz/real-estate-licensed-salesperson-reaa-2008/lorraine-barrett-8680/list-res', "Accept-Encoding" => "none"))
+doc = Nokogiri::HTML(open('http://marlboroughrealestate.co.nz/real-estate-licensed-salesperson-reaa-2008/michael-rea-8692/list-res', "Accept-Encoding" => "none"))
 
 listings = []
 
 doc.css('.listing').each do |l|
   link = l.css('.lazy').map { |link| link['href'] }
   desc = l.css('.desc h2 a').first.content
-  bed = l.css('.bed').first.content
-  bath = l.css('.bath').first.content
-  car = l.css('.car').first.content
-  land = l.css('.land').first.content
-  price = l.css('.price').first.content
+  l.search('.bed').any? ? bed = l.css('.bed').first.content : bed = ""
+  l.search('.bath').any? ? bath = l.css('.bath').first.content : bath = ""
+  l.search('.car').any? ? car = l.css('.car').first.content : car = ""
+  l.search('.land').any? ? land = l.css('.land').first.content : land = ""
+  if l.search('.price').any?
+    price = l.css('.price').first.content
+  elsif l.search('.auction-dt').any?
+    price = l.css('.auction-dt').first.content
+  end
+
   img = l.css('.lazy').map { |link| link['data-src']}
 
   # img.each do |style|
@@ -24,7 +29,7 @@ doc.css('.listing').each do |l|
   #   @v = v.join
   # end
 
-  listing = { "Link": link, "Image": img, "Address": desc, "Bed": bed, "Bath": bath, "Car": car, "Land": land, "Price": price }
+  listing = { "link": link, "image": img, "address": desc, "bed": bed, "bath": bath, "car": car, "land": land, "price": price }
   listings << listing
 end
 
